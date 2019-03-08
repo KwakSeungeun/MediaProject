5ee651dde77ba3f7a8cf8f5e15a20d4faea10665
KwakSeungeun <kseun0410@gmail.com>
2019-03-09 00:19:18 +0900

    Add Material Design

--- front/src/App.js
+++ front/src/App.js
@@ -1,25 +1,53 @@
 import React, { Component } from 'react';
-import logo from './logo.svg';
+import Dialog from '@material-ui/core/Dialog';
+import Button from '@material-ui/core/Button';
+import DialogActions from '@material-ui/core/DialogActions';
+import DialogContent from '@material-ui/core/DialogContent';
+import DialogContentText from '@material-ui/core/DialogContentText';
+import DialogTitle from '@material-ui/core/DialogTitle';
+import TextField from '@material-ui/core/TextField';
 import './App.css';
 
 class App extends Component {
+  state = {
+    open: true,
+  };
+
+  handleClose = () => {
+    this.setState({ open: false });
+  };
+
+  handleChange = (text) => {
+    console.log("TEST : ",text);
+  };
+  
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <p>
-            Edit <code>src/App.js</code> and save to reload.
-          </p>
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>
+      <div className="App-header">
+        <Dialog
+          open={this.state.open}
+          onClose={this.handleClose}
+          disableBackdropClick = {true}
+          fullWidth = {true}
+          aria-labelledby="alert-dialog-title"
+          aria-describedby="alert-dialog-description"
+        >
+          <DialogTitle id="alert-dialog-title">{"Cloud Service"}</DialogTitle>
+          <DialogContent>
+            <DialogContentText id="alert-dialog-description">
+              로그인 및 회원가입
+            </DialogContentText>
+            <TextField
+              label="ID를 입력하세요."
+              onChange={this.handleChange('name')}
+              margin="normal"
+              variant="outlined"
+            />
+          </DialogContent>
+          <DialogActions>
+            <Button onClick={this.handleClose} color="primary" autoFocus>닫기</Button>
+          </DialogActions>
+        </Dialog>
       </div>
     );
   }

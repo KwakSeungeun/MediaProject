4f78f345271dd64a3fd3efd1d824eb7abc82b69c
KwakSeungeun <kseun0410@gmail.com>
2019-04-21 00:40:12 +0900

    Topbar 가로배치 수정

--- front/src/components/TopBar.js
+++ front/src/components/TopBar.js
@@ -1,54 +1,44 @@
-import React from 'react';
-import PropTypes from 'prop-types';
-import { withStyles } from '@material-ui/core/styles';
-import AppBar from '@material-ui/core/AppBar';
+import React, { Component } from 'react';
+import '../index.css'
+import { sizing, spacing } from '@material-ui/system';
 import Toolbar from '@material-ui/core/Toolbar';
-import Button from '@material-ui/core/Button';
-import logo from './img/logo2.png'
-import profile from './img/profile.jpg'
-import Avatar from '@material-ui/core/Avatar';
-import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
+import logo from '../img/logo2.png';
+import { unstable_Box as Box } from '@material-ui/core/Box';
 
-const styles = {
-  avatar: {
-    margin: 10,
-    width: 60,
-    height: 60
-  },
-  img: {
-    width: 120,
-    height: 80,
-  }
-};
+class TopBar extends Component {
+    state = {
+        searchText : "",
+    }
 
-function TopBar(props) {
-
-    // var goHome = function() {
-    //     console.log('go home');
-    // }
+    goHome = () =>{
+        console.log("CLICK LOGO");
+    }
     
-    const { classes } = props;
-    return (
-        <div className={classes.root}>
-        {/* <AppBar position="static" color = "fafafa">       */}
-            <Toolbar>
-                <Grid container>
-                    <Grid item alignItems="flex-start">
-                        <Button><img src={logo} alt= "home" className={classes.img} /></Button>
-                    </Grid>
-                    <Grid item alignItems="center">사진 올려놓기</Grid>
-                    <Grid item alignItems="flex-end">
-                        <Button><Avatar alt="Sample" src={profile} className={classes.avatar} /></Button>
-                    </Grid>
-                </Grid>     
-            </Toolbar>
-        {/* </AppBar> */}
-        </div>
-  );
+    render() {
+        return (
+            <div>
+                <Toolbar className="row-container">
+                    {/* 로고 */}
+                    <div onClick={this.goHome} className="flex-1">
+                        <img src={logo} alt= "home"/>
+                    </div>
+                    {/* 검색 */}
+                    <div className="flex-6">
+                    <Box mx="auto"  width="75%">
+                        <Paper >
+                            <input placeholder="검색어를 입력하세요"></input>
+                        </Paper>
+                    </Box>
+                    </div>
+                    {/* 프로필 및 설정 */}
+                    <div className="flex-1">
+
+                    </div>
+                </Toolbar>
+            </div>
+      );
+    }
 }
 
-TopBar.propTypes = {
-  classes: PropTypes.object.isRequired,
-};
-
-export default withStyles(styles)(TopBar);
+export default TopBar;

3da0a33d57742c11f4724401ed186e92d06a5160
kwakseungeun <kseun0410@gmail.com>
2019-05-03 15:58:13 +0900

    수정

--- server/models/user.js
+++ server/models/user.js
@@ -5,6 +5,10 @@ module.exports = (sequelize, DataTypes) => {
             primaryKey: true,
             autoIncrement: true
         },
+        name:{
+            type: DataTypes.STRING,
+            allowNull: false
+        },
         email:{
             type: DataTypes.STRING,
             allowNull: false

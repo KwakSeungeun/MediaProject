d3c3fef434802208ca0c6bac6fa4a6bd41e2c310
kwakseungeun <kseun0410@gmail.com>
2019-05-19 14:43:48 +0900

    Create Photo Search Modal

---
+++ front/src/components/PhotoSearch.js
@@ -0,0 +1,11 @@
+import React, { Component } from 'react';
+
+class PhotoSearch extends Component {
+    render(){
+        return(
+            <div>TEST!</div>
+        );
+    }
+}
+
+export default PhotoSearch;

9048ef3d4d1551dfc35ad23ef15301fd6320ff6a
PARKJINYONG <augustparkjy@gmail.com>
2019-05-23 21:07:52 +0900

    for pull

--- server/routes/search/index.js
+++ server/routes/search/index.js
@@ -2,5 +2,5 @@ const router = require('express').Router();
 const controller = require('./search.controller');
 
 router.post('/face/detection', controller.faceDetection);
-
+router.get('/face/comparison', controller.faceComparison)
 module.exports = router;
